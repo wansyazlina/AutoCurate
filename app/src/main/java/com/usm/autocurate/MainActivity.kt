e76cdfcd1087c8601d927a8402511fffcc54603c
wansyazlina <syazlina2008@gmail.com>
2021-03-17 13:06:31 +0800

    added kotlin extensions to avoid using findviewbyid

--- app/src/main/java/com/usm/autocurate/MainActivity.kt
+++ app/src/main/java/com/usm/autocurate/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.usm.autocurate
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.usm.autocurate.R
+import kotlinx.android.synthetic.main.splashscreen.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.splashscreen)
+
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        val typeface : Typeface = Typeface.createFromAsset(assets,
+                "Montserrat-Regular")
+
+
+
+
+       
+
     }
 }
--- app/src/test/java/com/usm/autocurate/ExampleUnitTest.java
+++ app/src/test/java/com/usm/autocurate/ExampleUnitTest.java
@@ -1,6 +1,6 @@
 package com.usm.autocurate;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.*;
 
