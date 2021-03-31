1bb5f1c2860413566529cb6d13fe92c0774124f7
wansyazlina <syazlina2008@gmail.com>
2021-04-01 02:10:07 +0800

    added login and splashscreen

---
+++ app/src/main/java/com/usm/autocurate/ui/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.usm.autocurate.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.usm.autocurate.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+}
--- app/src/main/java/com/usm/autocurate/ui/auth/SplashActivity.kt
+++ app/src/main/java/com/usm/autocurate/ui/auth/SplashActivity.kt
@@ -1,6 +1,44 @@
 package com.usm.autocurate.ui.auth
 
+import android.content.Intent
+import android.net.sip.SipErrorCode.TIME_OUT
+import android.os.Bundle
+import android.os.Handler
+import android.os.PersistableBundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import com.usm.autocurate.R
 
-class SplashActivity : AppCompatActivity {
+class SplashActivity: AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splashscreen)
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        loadSplashScreen()
+
+
+    }
+
+
+
+    val TIME_OUT: Long = 3000
+
+    fun loadSplashScreen(){
+        Handler().postDelayed({
+            // You can declare your desire activity here to open after finishing splash screen. Like MainActivity
+            val intent = Intent(this, LoginActivity::class.java).apply {
+                putExtra(EXTRA_MESSAGE, "hello")
+            }
+            startActivity(intent)
+          //  finish()
+        },TIME_OUT)
+    }
+
 }
